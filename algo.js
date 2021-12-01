cedfbcfa9a3f2bd0c4e1c26dfec37e4ef99408e6
Tony_M <tony.mazzariol@gmail.com>
2021-12-01 17:05:47 +0100

    version_1.1

--- algo.js
+++ algo.js
@@ -1,28 +1,35 @@
 var colors = require('colors');
 const people = require('./people');
 const tr = require('./trinker');
+const br = require('./test');
 
 console.log(tr.title());
 console.log("Model des données : ");
 console.log(people[0]);
 console.log(tr.line('LEVEL 1'));
+
+console.log(" test : ", br.test(people));
+
 console.log("Nombre d'hommes : ",                                                                   tr.nbOfMale(people));
 console.log("Nombre de femmes : ",                                                                  tr.nbOfFemale(people));
 console.log("Nombre de personnes qui cherchent un homme :",                                         tr.nbOfMaleInterest(people));
 console.log("Nombre de personnes qui cherchent une femme :",                                        tr.nbOfFemaleInterest(people));
-console.log("Nombre de personnes qui gagnent plus de 2000$ :",                                      "create function".blue);
-console.log("Nombre de personnes qui aiment les Drama :",                                           "create function".blue);
-console.log("Nombre de femmes qui aiment la science-fiction :",                                     "create function".blue);
+console.log("Nombre de personnes qui gagnent plus de 2000$ :",                                      tr.nBOfPeopleGettingMore2000(people));
+console.log("Nombre de personnes qui aiment les Drama :",                                           tr.nBOfPeopleLikeDrama(people));
+console.log("Nombre de personnes qui aiment la science-fiction :",                                  tr.nBOfPeopleLikeSf(people));
+console.log("Nombre de femmes qui aiment la science-fiction :",                                     tr.nBOfFemaleLikeSf(people));
 console.log(tr.line('LEVEL 2'));
-console.log("Nombre de personnes qui aiment les documentaires et gagnent plus de 1482$ :",            "create function".blue);
-console.log("Liste des noms, prénoms, id et revenu des personnes qui gagnent plus de 4000$ :",      "create function".blue);
-console.log("Homme le plus riche (nom et id) :",                                                    "create function".blue);
-console.log("Salaire moyen :",                                                                      "create function".blue);
-console.log("Salaire médian :",                                                                     "create function".blue);
-console.log("Nombre de personnes qui habitent dans l'hémisphère nord :",                             "create function".blue);
-console.log("Salaire moyen des personnes qui habitent dans l'hémisphère sud :",                      "create function".blue);
+//console.log("Nombre de personnes qui gagnent plus de 1482$ :",                                      tr.nBoPeopleGettingMore1482(people));
+// console.log("Nombre de personnes qui aiment les documentaires  :",                                  tr.nBoPeopleLikeDoc(people));
+console.log("Nombre de personnes qui aiment les documentaires et gagnent plus de 1482$ :",          tr.nBoPeopleGet1482andLikeDoc(people ));
+console.log("Liste des noms, prénoms, id et revenu des personnes qui gagnent plus de 4000$ :",      tr.nBOfPeopleGettingMore4000(people));
+console.log("Homme le plus riche (nom et id) :",                                                    tr.richestMan(people));
+console.log("Salaire moyen :",                                                                      tr.averageSalary(people));
+console.log("Salaire médian :",                                                                     tr.medianSalary(people));
+console.log("Nombre de personnes qui habitent dans l'hémisphère nord :",                            tr.nBOfPeopleinNorth(people));
+console.log("Salaire moyen des personnes qui habitent dans l'hémisphère sud :",                     tr.averageSalaryInSounth(people));
 console.log(tr.line('LEVEL 3'));            
-console.log("Personne qui habite le plus près de Bérénice Cawt (nom et id) :",                      "create function".blue);
+console.log("Personne qui habite le plus près de Bérénice Cawt (nom et id) :",                      tr.closestToberenice(people));
 console.log("Personne qui habite le plus près de Ruì Brach (nom et id) :",                          "create function".blue);
 console.log("les 10 personnes qui habite les plus près de Josée Boshard (nom et id) :",              "create function".blue);
 console.log("Les noms et ids des 23 personnes qui travaillent chez google :",                       "create function".blue);
@@ -41,16 +48,19 @@ préférence de film en commun (afficher les deux et la distance entre les deux)
 console.log("Liste des couples femmes / hommes qui ont les même préférences de films :",            "create function".blue);
 console.log(tr.line('MATCH'));
 /* 
-    On match les gens avec ce qu'ils cherchent (homme ou femme).
-    On prend en priorité ceux qui sont les plus proches.
-    Puis ceux qui ont le plus de goût en commun.
-    Pour les couples hétéroséxuel on s'assure que la femme gagne au moins 10% de moins que l'homme mais plus de la moitié.
-    Les gens qui travaillent chez google ne peuvent qu'être en couple entre eux.
-    Quelqu'un qui n'aime pas les Drama ne peux pas être en couple avec quelqu'un qui les aime.
-    Quelqu'un qui aime les films d'aventure doit forcement être en couple avec quelqu'un qui aime aussi les films d'aventure.
-    Le différences d'age dans un couple doit être inférieure à 25% (de l'age du plus agée des deux)
-    ߷    ߷    ߷    Créer le plus de couples possibles.   ߷    ߷    ߷    
-    ߷    ߷    ߷    Mesurez le temps de calcul de votre fonction   ߷    ߷    ߷    
-    ߷    ߷    ߷    Essayez de réduire le temps de calcul au maximum   ߷    ߷    ߷    
+On match les gens avec ce qu'ils cherchent (homme ou femme).
+On prend en priorité ceux qui sont les plus proches.
+Puis ceux qui ont le plus de goût en commun.
+Pour les couples hétéroséxuel on s'assure que la femme gagne au moins 10% de moins que l'homme mais plus de la moitié.
+Les gens qui travaillent chez google ne peuvent qu'être en couple entre eux.
+Quelqu'un qui n'aime pas les Drama ne peux pas être en couple avec quelqu'un qui les aime.
+Quelqu'un qui aime les films d'aventure doit forcement être en couple avec quelqu'un qui aime aussi les films d'aventure.
+Le différences d'age dans un couple doit être inférieure à 25% (de l'age du plus agée des deux)
+߷    ߷    ߷    Créer le plus de couples possibles.   ߷    ߷    ߷    
+߷    ߷    ߷    Mesurez le temps de calcul de votre fonction   ߷    ߷    ߷    
+߷    ߷    ߷    Essayez de réduire le temps de calcul au maximum   ߷    ߷    ߷    
 */
+
 console.log("liste de couples à matcher (nom et id pour chaque membre du couple) :",             tr.match(people));
+
+
--- trinker.js
+++ trinker.js
@@ -1,3 +1,5 @@
+const { indexOf, includes } = require("./people")
+
 module.exports = {
     title: function (){
         return `
@@ -18,11 +20,29 @@ $$$$$$$$\\        $$\\           $$\\                           $$\\
     },
 
     allMale: function(p){
-        return "not implemented".red;
+        /* 
+        let count = 0
+        p.forEach(element => {
+            if (element.gender === "Male"){
+                count ++
+            }
+        }); 
+        return console.log(count) 
+        */
+        return p.filter( obj => obj.gender === "Male");
     },
 
     allFemale: function(p){
-        return "not implemented".red;
+        /*  
+        let count = 0
+        p.forEach(element => {
+            if (element.gender === "Female"){
+                count ++
+            }
+        }); 
+        return console.log(count) 
+        */
+        return p.filter( index => index.gender === "Female");
     },
 
     nbOfMale: function(p){
@@ -30,16 +50,111 @@ $$$$$$$$\\        $$\\           $$\\                           $$\\
     },
 
     nbOfFemale: function(p){
-        return "not implemented".red;
+        return this.allFemale(p).length
     },
 
     nbOfMaleInterest: function(p){
-        return "not implemented".red;
+        return (p.filter( index => index.looking_for === "M")).length;
     },
 
     nbOfFemaleInterest: function(p){
-        return "not implemented".red;
+        return (p.filter( index => index.looking_for === "F")).length;
     },
+
+    nBOfPeopleGettingMore2000: function(p){
+        return (p.filter( index => index.income.substring(1) >= 2000.00)).length;
+    },
+
+    nBOfPeopleLikeDrama: function(p){
+        return (p.filter( index => (index.pref_movie).includes("Drama"))).length
+    },
+    
+    nBOfPeopleLikeSf: function(p){
+        return (p.filter( index => (index.pref_movie).includes("Sci-Fi"))).length
+    },
+    
+    nBOfFemaleLikeSf: function(p){
+        let temp = this.allFemale(p)
+        return (temp.filter( index => (index.pref_movie).includes("Sci-Fi"))).length
+    },
+
+    /* LEVEL 2 */
+
+    nBoPeopleLikeDoc: function(p){
+        return (p.filter( index => (index.pref_movie).includes("Documentary")))
+    }, 
+
+    nBoPeopleGettingMore1482: function(p){
+        return (p.filter( index => index.income.substring(1) > 1482.00));
+    }, 
+    
+    nBoPeopleGet1482andLikeDoc: function(p){
+        let temp = this.nBoPeopleLikeDoc(p)
+        return this.nBoPeopleGettingMore1482(temp).length
+    }, 
+
+    nBOfPeopleGettingMore4000: function(p){
+        let group = []
+        temp = (p.filter( index => index.income.substring(1) >= 4000.00));
+        temp.forEach(element => {
+            group.push("nom : " + element.last_name + ", prenom : " + element.first_name + ", id : " + element.id + ",income : " + element.income);
+        });
+        return group
+    },
+
+    richestMan: function(p){
+        array = this.allMale(p)
+        array.sort((a, b) => parseFloat(b.income.substring(1)) - parseFloat(a.income.substring(1)));
+        return (array[0].first_name +" "+ array[0].last_name + " id : " + array[0].id).yellow
+    },
+
+    averageSalary: function(p){
+        let total = 0
+        p.forEach(element => {
+            total = total + parseFloat(element.income.substring(1))
+            
+        });
+        return ((total/p.length).toFixed(2)+ " $").yellow
+    },
+
+    medianSalary: function(p){
+        let median = 0
+        p = p.sort((a, b) => parseFloat(b.income.substring(1)) - parseFloat(a.income.substring(1)));
+        if (p.length % 2 == 0) {
+            //pair
+            median = ( parseFloat(p[(p.length/2)].income.substring(1)) + parseFloat(p[(p.length/2)].income.substring(1)) )/2
+        } else if (p.length % 2 != 0) {
+            // impair
+            median = parseFloat(p[(p.length/2)].income.substring(1))
+        }
+        return median
+    },
+
+    nBOfPeopleinNorth: function(p) {
+       return (p.filter( index => index.latitude > 0)).length
+    },
+    
+    PeopleinSouth: function(p) {
+       return (p.filter( index => index.latitude < 0))
+    },
+
+    averageSalaryInSounth: function(p) {
+        let temp = this.PeopleinSouth(p)
+        return this.averageSalary(temp)
+    },
+
+    /* LEVEL 3 */
+
+    // closest people to Bérénice Cawt (name and id)
+    closestToberenice: function(p){
+        let Berenice = p.filter( index => index.first_name == "Bérénice")
+        MrsCawt_B = Berenice.filter( index => index.last_name == "Cawt")
+        l = p.splice(MrsCawt_B[0].id, 0, )
+        return MrsCawt_B[0].id
+    },
+
+
+    /* MATCH */
 
     match: function(p){
         return "not implemented".red;
